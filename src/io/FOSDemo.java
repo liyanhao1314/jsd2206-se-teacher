a0d844d25a6de2f2fc4941fa226d0e17395e597c
Xiloer <83123095@qq.com>
2022-07-28 10:11:24 +0800

    day02

--- src/io/FOSDemo.java
+++ src/io/FOSDemo.java
@@ -2,6 +2,7 @@ package io;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * JAVA IO 输入与输出  Input和Output
@@ -25,11 +26,41 @@ import java.io.FileOutputStream;
  * java.io.FileOutputStream继承自OutputStream，里面定义了写出字节的相关方法。
  */
 public class FOSDemo {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         //向当前项目目录中的文件fos.dat中写入字节数据
         //上述需求中的关键词：文件，写  应当使用文件输出流。
         FileOutputStream fos = new FileOutputStream("./fos.dat");//alt+回车  选择Add exception....
+        /*
+            java.io.OutputStream超类上定义了写出一个字节的方法:
+            void write(int d)
+            写出一个字节，写出的是给定的int值对应的2进制的"低八位"
 
+            文件流实现了上述方法，作用是向文件中写入1个字节。
+         */
+        /*
+            int型的1对应的2进制:00000000 00000000 00000000 00000001
+            write(1):写出的1对应2进制的"低八位"
+                                       vvvvvvvv
+            00000000 00000000 00000000 00000001
+                                       写出的字节
 
+            write方法调用完毕后，fos.dat中的样子:
+            00000001
+         */
+        fos.write(1);
+        /*
+            int型的3对应的2进制:00000000 00000000 00000000 00000011
+            write(3):写出的3对应2进制的"低八位"
+                                       vvvvvvvv
+            00000000 00000000 00000000 00000011
+                                       写出的字节
+
+            write方法调用完毕后，fos.dat中的样子:
+            00000001 00000011
+         */
+        fos.write(3);
+
+        System.out.println("写出完毕!");
+        fos.close();//流使用完毕要关闭来释放资源
     }
 }
