58afa672d0f768aa36386ebfc7eab548c0a8286c
Xiloer <83123095@qq.com>
2022-07-28 11:33:42 +0800

    day02

---
+++ src/io/CopyDemo.java
@@ -0,0 +1,42 @@
+package io;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * 使用文件流完成文件的复制操作
+ */
+public class CopyDemo {
+    public static void main(String[] args) throws IOException {
+        FileInputStream fis = new FileInputStream("image.jpg");
+        FileOutputStream fos = new FileOutputStream("image_cp.jpg");
+        /*
+            image.jpg文件内容
+            11001100 00110011 11110000 00001111 10101010 01010101 ...
+
+            第1次调用:d = fis.read();
+            11001100 00110011 11110000 00001111 10101010 01010101 ...
+            ^^^^^^^^
+            读取的字节
+            d的2进制:00000000 00000000 00000000 11001100
+
+            fos.write(d);
+            d:00000000 00000000 00000000 11001100
+                                         ^^^^^^^^
+                                         写出的字节
+            image_cp.jpg文件数据:
+            11001100
+         */
+        int d;//记录每次读取的字节内容
+        while(  (d = fis.read()) != -1    ) {//若读取到了-1则应当停止循环，没有读取到-1则应当抄
+            fos.write(d);
+        }
+
+
+        System.out.println("复制完毕!");
+        fos.close();
+        fis.close();
+    }
+}
