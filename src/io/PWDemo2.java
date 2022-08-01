a958f6f320a39329d73b4279fa19625106d68a92
Xiloer <83123095@qq.com>
2022-08-01 14:35:49 +0800

    day04

--- src/io/PWDemo2.java
+++ src/io/PWDemo2.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
  * 自行完成流连接的创建
  */
 public class PWDemo2 {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         //向文件中按行写入文本数据
         //1创建文件流(低级流，字节流):向文件中写入字节数据
         FileOutputStream fos = new FileOutputStream("pw2.txt");//如果希望对文件追加写，要在这里打开追加模式
@@ -17,7 +17,15 @@ public class PWDemo2 {
         //3创建缓冲字符流(高级流，字符流):块写文本数据加速
         BufferedWriter bw = new BufferedWriter(osw);
         //4创建PrintWriter(高级流，字符流):按行写出字符串，自动行刷新
-        PrintWriter pw = new PrintWriter(bw);
+        /*
+            当我们主动将PrintWriter连接在一个流上时(构造方法第一个参数为连接在哪个流上)
+            那么PrintWriter的构造器就支持再传入一个boolean型参数的重载，如果这个值为
+            true时，则打开了自动行刷新功能。
+            所谓自动行刷新:
+            当我们使用println方法写出一行字符串时，会自动进行一次flush动作。
+            注意:调用write，print方法是不会自动flush的。
+         */
+        PrintWriter pw = new PrintWriter(bw,true);
 
         /*
             实现简易记事本功能。
@@ -32,10 +40,16 @@ public class PWDemo2 {
                 字面量.equals(变量)的形式。因为字面量一定非空，可以避免空指针的产生
              */
             if("exit".equals(line)){
-//            if(line.equals("exit")){
                 break;//如果用户输入的是退出，则停止循环
             }
             pw.println(line);
+            /*
+                java中所有的输出流(无论是字符还是字节流)都支持flush方法。
+                不过只有缓冲流的flush是真实将自身缓冲区中缓存的数据做强制写出的。
+                而其他的高级流中flush方法的实现仅仅是调用了其连接的流的flush，将
+                该动作传递下去。
+             */
+//            pw.flush();
         }
 
         System.out.println("写出完毕");
