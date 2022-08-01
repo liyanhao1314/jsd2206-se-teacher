e9f3760e35c4aa16eece924cd64cb267c0886bbd
Xiloer <83123095@qq.com>
2022-08-01 15:17:06 +0800

    day04

--- src/io/BRDemo.java
+++ src/io/BRDemo.java
@@ -1,7 +1,9 @@
 package io;
 
-import java.io.*;
-import java.nio.charset.StandardCharsets;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * 缓冲字符输入流
@@ -19,10 +21,14 @@ public class BRDemo {
         /*
             BufferedReader提供了一个方法:
             String readLine()
-            该方法可以读取一行字符串。
+            该方法可以读取一行字符串。并将这行内容返回(返回的内容最后不含有换行符)，
+            如果该行为空行(只有换行符)，那么方法会返回一个空字符串
+            如果流读取到了末尾，则返回值为null
          */
-        String line = br.readLine();
-        System.out.println(line);
+        String line;
+        while((line = br.readLine()) != null){
+            System.out.println(line);
+        }
 
         br.close();
     }
