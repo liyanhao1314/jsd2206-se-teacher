3e39bad48d1c59ef63ac02240212adb18b255e14
jason <qlfreesky@gmail.com>
2022-08-02 09:34:35 +0800

    day05

--- src/exception/FinallyDemo.java
+++ src/exception/FinallyDemo.java
@@ -8,4 +8,23 @@ package exception;
  * 通常我们使用finally去完成资源释放这类操作,比如IO中的关闭流操作.
  */
 public class FinallyDemo {
+    public static void main(String[] args) {
+        System.out.println("程序开始了");
+        try {
+            String str = "null";
+            System.out.println(str.length());
+            return;
+        } catch (Exception e) {
+            System.out.println("出错了");
+        }finally {
+            System.out.println("finally中的代码执行了");
+        }
+        System.out.println("程序结束了");
+    }
 }
+
+
+
+
+
+
